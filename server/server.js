cec328cc7bddb19a14b9f7e91f44e61117c04eca
Paul philips <shahid@gmail.com>
2026-09-13 13:40:33 +0530

    Configure Render deployment

--- server/server.js
+++ server/server.js
@@ -1,11 +1,58 @@
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import authRoutes from "./routes/authRoutes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+
+// // Middleware
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173"
+//   })
+// );
+
+// app.use(express.json());
+
+
+// // Test
+// app.get("/", (req, res) => {
+//   res.json({
+//     success: true,
+//     message: "Lost & Found Backend is running"
+//   });
+// });
+
+
+// // Auth API
+// app.use("/api/auth", authRoutes);
+
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
+
+
+// Get current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 // Middleware
@@ -18,8 +65,8 @@ app.use(
 app.use(express.json());
 
 
-// Test
-app.get("/", (req, res) => {
+// Test API
+app.get("/api", (req, res) => {
   res.json({
     success: true,
     message: "Lost & Found Backend is running"
@@ -29,6 +76,18 @@ app.get("/", (req, res) => {
 
 // Auth API
 app.use("/api/auth", authRoutes);
+
+
+// Serve React Frontend
+const clientPath = path.join(__dirname, "../client/dist");
+
+app.use(express.static(clientPath));
+
+
+// React routes
+app.get("*splat", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
 
 
 const PORT = process.env.PORT || 5000;
